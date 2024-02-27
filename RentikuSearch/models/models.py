7857095d5bcd5b051c58f3df077ae7a392b009d8
Blackbeing <johnjahi55@gmail.com>
2024-02-27 21:04:42 +0300

    feat Create database models (User, Property)

---
+++ RentikuSearch/models/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Text, Float, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50),
+                                          nullable=False,
+                                          unique=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+
+class Property(Base):
+    __tablename__ = "properties"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50))
+    type: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(Text)
+    location: Mapped[str] = mapped_column(String(50))
+    price: Mapped[float] = mapped_column(Float)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
