e845ff05221efa43f9236c9ab492ea7a2a56ad59
Blackbeing <johnjahi55@gmail.com>
2024-03-04 13:20:46 +0300

    feat String representation of objects

--- RentikuSearch/models/base_model.py
+++ RentikuSearch/models/base_model.py
@@ -17,3 +17,8 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
+
+    def __repr__(self):
+        return "[{}:{}] {}".format(
+            self.__class__.__name__, self.id, self.to_dict()
+        )
