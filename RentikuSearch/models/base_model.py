6f5302e3371e3aba2d6d312c6f8f9246d3f119ef
Blackbeing <johnjahi55@gmail.com>
2024-03-04 10:06:50 +0300

    feat Convert objects to dict

--- RentikuSearch/models/base_model.py
+++ RentikuSearch/models/base_model.py
@@ -10,3 +10,10 @@ class BaseModel:
     def save(self):
         models.storage.new(self)
         models.storage.save()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
