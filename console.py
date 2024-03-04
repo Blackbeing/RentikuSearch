b5088e758c302504c982b23b3b2c89fe6c2560d0
Blackbeing <johnjahi55@gmail.com>
2024-03-04 11:46:23 +0300

    feat Show property from console

--- console.py
+++ console.py
@@ -11,48 +11,59 @@ storage.reload()
 MODELS_DICT = {"property": Property, "user": User}
 
 base_parser = cmd2.Cmd2ArgumentParser(description="RentikuSearch CLI")
-base_subparsers = base_parser.add_subparsers(
-    title="subcommands", help="subcommands help"
-)
+base_subparsers = base_parser.add_subparsers(title="subcommands",
+                                             help="subcommands help")
 create_parser = base_subparsers.add_parser("create", help="Create Object")
-create_subparsers = create_parser.add_subparsers(
-    title="subcommands", help="subcommands help"
-)
+create_subparsers = create_parser.add_subparsers(title="subcommands",
+                                                 help="subcommands help")
 create_user_parser = create_subparsers.add_parser("user", help="Create user")
 
-create_user_parser.add_argument(
-    "-u", "--username", required=True, help="username"
-)
+create_user_parser.add_argument("-u",
+                                "--username",
+                                required=True,
+                                help="username")
 create_user_parser.add_argument("-e", "--email", required=True, help="email")
 create_user_parser.add_argument("-p", "--password", help="password")
 
-create_property_parser = create_subparsers.add_parser(
-    "property", help="Create property"
-)
+create_property_parser = create_subparsers.add_parser("property",
+                                                      help="Create property")
 
 create_property_parser.add_argument("-T", "--title", help="Property title")
 create_property_parser.add_argument("-t", "--type", help="Property type")
-create_property_parser.add_argument(
-    "-d", "--description", help="Property Description"
-)
-create_property_parser.add_argument(
-    "-l", "--location", help="Property location"
-)
-create_property_parser.add_argument(
-    "-p", "--price", type=float, help="Property price"
-)
-create_property_parser.add_argument(
-    "-u", "--owner-id", type=int, help="Property owner"
-)
+create_property_parser.add_argument("-d",
+                                    "--description",
+                                    help="Property Description")
+create_property_parser.add_argument("-l",
+                                    "--location",
+                                    help="Property location")
+create_property_parser.add_argument("-p",
+                                    "--price",
+                                    type=float,
+                                    help="Property price")
+create_property_parser.add_argument("-u",
+                                    "--owner-id",
+                                    type=int,
+                                    help="Property owner")
 show_parser = base_subparsers.add_parser("show", help="Show objects")
-show_subparsers = show_parser.add_subparsers(
-    title="subcommands", help="subcommands help"
-)
+show_subparsers = show_parser.add_subparsers(title="subcommands",
+                                             help="subcommands help")
 show_user_parser = show_subparsers.add_parser("user", help="Show Users")
-show_user_parser.add_argument(
-    "-a", "--all", action="store_true", help="Show all users"
-)
+show_user_parser.add_argument("-a",
+                              "--all",
+                              action="store_true",
+                              help="Show all users")
 show_user_parser.add_argument("-i", "--id", help="Show user by id")
+
+show_parser = base_subparsers.add_parser("show", help="Show objects")
+show_subparsers = show_parser.add_subparsers(title="subcommands",
+                                             help="subcommands help")
+show_property_parser = show_subparsers.add_parser("property",
+                                                  help="Show property")
+show_property_parser.add_argument("-a",
+                                  "--all",
+                                  action="store_true",
+                                  help="Show all properties")
+show_property_parser.add_argument("-i", "--id", help="Show property by id")
 
 
 class RentikuSearchCMD(cmd2.Cmd):
@@ -116,6 +127,14 @@ class RentikuSearchCMD(cmd2.Cmd):
             print(storage.get(User, arg.id))
 
     show_user_parser.set_defaults(func=show_user)
+
+    def show_property(self, arg):
+        if arg.all:
+            print(storage.all(Property))
+        elif arg.id:
+            print(storage.get(Property, arg.id))
+
+    show_property_parser.set_defaults(func=show_property)
 
     @cmd2.with_argparser(create_parser)
     def do_create(self, args):
