1c41aa1fedbf4a536dffb32f86bb58f3e34b9715
Blackbeing <johnjahi55@gmail.com>
2024-03-04 10:11:12 +0300

    tests Add tests

---
+++ tests/api/v1/views/test_user.py
@@ -0,0 +1,36 @@
+import unittest
+
+from config import get_config
+from RentikuSearch.api.v1.app import create_app
+
+
+class TestUserEndpoint(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ap = create_app(get_config())
+        cls.app = cls.ap.test_client()
+
+    def test_create_user(self):
+        data = {
+            "username": "John",
+            "email": "John@email",
+            "password_hash": "password",
+        }
+        response = self.app.post("/api/v1/user", json=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_all_users(self):
+        response = self.app.get("/api/v1/user")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json)
+
+    def test_get_user_by_id(self):
+        params = {"id": "1"}
+        response = self.app.get("/api/v1/user", query_string=params)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json)
+
+
+if __name__ == "__main__":
+    unittest.main()
