ca61ba9794a5d52f7c93bd0c550cfc5cd7bcf0ca
Blackbeing <johnjahi55@gmail.com>
2024-03-04 11:43:05 +0300

    refactor get user by id

--- RentikuSearch/api/v1/views/user.py
+++ RentikuSearch/api/v1/views/user.py
@@ -1,10 +1,14 @@
-import unittest
-
 from flask import jsonify, make_response, request
 
 from RentikuSearch.api.v1.views import bp
 from RentikuSearch.models import storage
 from RentikuSearch.models.models import User
+
+
+@bp.route("/user/<id>", methods=["GET"], strict_slashes=False)
+def get_user_by_id(id):
+    return_val = storage.get(User, id).to_dict()
+    return make_response(jsonify(return_val), 200)
 
 
 @bp.route("/user", methods=["GET", "POST"], strict_slashes=False)
@@ -14,18 +18,8 @@ def users():
         if data:
             user = User(**data)
             user.save()
-        return make_response(jsonify({}), 201)
+        return make_response(jsonify(user.to_dict()), 201)
 
     elif request.method == "GET":
         return_val = [user.to_dict() for user in storage.all(User)]
         return make_response(jsonify(return_val), 200)
-
-
-@bp.route("/user/<id>", methods=["GET"], strict_slashes=False)
-def get_user_by_id(id):
-    return_val = storage.get(User, id)
-    return make_response(jsonify(return_val), 200)
-
-
-if __name__ == "__main__":
-    unittest.main()
