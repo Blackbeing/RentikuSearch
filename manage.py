c6e24224cfa0354e492fb35deaac11b6071d7435
Blackbeing <johnjahi55@gmail.com>
2024-03-06 21:28:47 +0300

    feat Run web app from manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,27 @@
 import asyncio
+import sys
 
 import uvicorn
 
 
-async def main():
+async def run_api():
     uvicorn.run(
         "RentikuSearch.api.v1.app:app", host="0.0.0.0", port=8000, reload=True
     )
 
 
+async def run_web():
+    uvicorn.run(
+        "RentikuSearch.web_fastapi.index:app",
+        host="0.0.0.0",
+        port=5050,
+        reload=True,
+    )
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    choice = sys.argv[1]
+    if choice == "api":
+        asyncio.run(run_api())
+    elif choice == "web":
+        asyncio.run(run_web())
