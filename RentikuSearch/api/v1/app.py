0bdfe28953b8b59ef74186c31422203a61756a19
Blackbeing <johnjahi55@gmail.com>
2024-03-06 12:49:20 +0300

    WIP Start fastapi migration

--- RentikuSearch/api/v1/app.py
+++ RentikuSearch/api/v1/app.py
@@ -1,37 +1,8 @@
-from flask import Flask
+from fastapi import FastAPI
 
-from config import BaseConfig
-from RentikuSearch.api.v1.views import bp
-from RentikuSearch.models import storage
+app = FastAPI()
 
 
-def create_app(config_class=BaseConfig):
-    """
-    Create and configure an instance of the Flask application
-    Arguments:
-        config_class: Configuration class
-    Returns:
-        app: Flask application
-    """
-    app = Flask(__name__)
-    app.config.from_object(config_class())
-
-    # test url
-    app.register_blueprint(bp)
-
-    @app.route("/test/")
-    def test_page():
-        return "<h1>Flask Factory Pattern</h1>"
-
-    @app.teardown_request
-    def close_session(req):
-        storage.disconnect()
-
-    return app
-
-
-if __name__ == "__main__":
-    from config import get_config
-
-    app = create_app(get_config())
-    app.run()
+@app.get("/test")
+async def test_page():
+    return {"message": "Hello world"}
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
-from config import get_config
-from RentikuSearch.api.v1.app import create_app
+import asyncio
+
+import uvicorn
 
 
-def main():
-    app = create_app(get_config())
-    app.run()
+async def main():
+    uvicorn.run(
+        "RentikuSearch.api.v1.app:app", host="0.0.0.0", port=8000, reload=True
+    )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
