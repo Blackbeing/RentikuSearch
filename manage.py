6bf6136476f6827cfc92a45d275f897e6206b718
Blackbeing <johnjahi55@gmail.com>
2024-03-07 17:18:21 +0300

    refactor Rename web_fastapi app

--- manage.py
+++ manage.py
@@ -5,16 +5,17 @@ import uvicorn
 
 
 async def run_api():
-    uvicorn.run(
-        "RentikuSearch.api.v1.app:app", host="0.0.0.0", port=8000, reload=True
-    )
+    uvicorn.run("RentikuSearch.api.v1.app:app",
+                host="0.0.0.0",
+                port=5050,
+                reload=True)
 
 
 async def run_web():
     uvicorn.run(
-        "RentikuSearch.web_fastapi.index:app",
+        "RentikuSearch.web_fastapi.app:app",
         host="0.0.0.0",
-        port=5050,
+        port=8000,
         reload=True,
     )
 
