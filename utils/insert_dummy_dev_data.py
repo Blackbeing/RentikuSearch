c26058116aa03c9e6af85d24943c5168e39300f0
Blackbeing <johnjahi55@gmail.com>
2024-03-16 12:00:11 +0300

    refactor Update dummy data script to use faker

--- utils/insert_dummy_dev_data.py
+++ utils/insert_dummy_dev_data.py
@@ -1,38 +1,73 @@
-# can be run from base dir using
-# python -m utils.insert_dummy_dev_data
-import json
+#!/usr/bin/python
+
+import random
 from pathlib import Path
 
+from faker import Faker
+
+from RentikuSearch import dependancies as dp
 from RentikuSearch.models.database import Database
 from RentikuSearch.models.models import Property, User
 
-base_path = Path(__file__).parent
+fake = Faker()
 
 db_c = Database()
 db_c.drop_tables()
 db_c.init_db()
-db = db_c.session()
+db = db_c.session
 
 
-def load_data(file_path):
-    with file_path.open() as fd:
-        data = json.load(fd)
-        properties = data.get("properties", [])
-        users = data.get("users", [])
-
-        for user_dict in users:
-            user_dict.pop("id")
-            user = User(**user_dict)
-            db.add(user)
-        db.commit()
-
-        for property_dict in properties:
-            property_dict.pop("id")
-            property = Property(**property_dict)
-            db.add(property)
-        db.commit()
+def get_img_url():
+    property_images = list(
+        (
+            Path(__file__).parent.parent
+            / "RentikuSearch"
+            / "web_fastapi"
+            / "static"
+            / "images"
+            / "properties"
+        ).iterdir()
+    )
+    image_url = random.choice(property_images)
+    p = "/".join(image_url.parts[-3:])
+    image_url = "/{}".format(p)
+    return image_url
 
 
-db_json_file = base_path / "db.json"
+def create_user_and_property():
+    fn = fake.first_name()
+    ln = fake.last_name()
+    username = "{}_{}".format(fn, ln)
+    email = "{}@{}".format(username, fake.domain_name())
+    password = dp.hash_password(fake.password())
+    user = User(username=username, email=email, password=password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
 
-load_data(db_json_file)
+    property = Property(
+        title=fake.company(),
+        description=fake.catch_phrase(),
+        action=["sell", "rent", "lease"][
+            random.randint(0, 2)
+        ],  # Pick random action
+        price=fake.pyint(min_value=10000, max_value=1000000),
+        rent=fake.pyint(min_value=1000, max_value=5000),
+        num_rooms=fake.pyint(min_value=1, max_value=5),
+        address=fake.address(),
+        latitude=str(fake.latitude()),
+        longitude=str(fake.longitude()),
+        region=fake.country(),
+        image_url=get_img_url(),
+        owner_id=user.id,  # Link property to user
+    )
+    db.add(property)
+    db.commit()
+    db.refresh(property)
+
+    return user, property
+
+
+if __name__ == "__main__":
+    for _ in range(10):
+        create_user_and_property()
