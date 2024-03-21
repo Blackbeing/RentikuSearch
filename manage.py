643e3281f19b372bd59f2621f98f797335b584d0
Blackbeing <johnjahi55@gmail.com>
2024-03-21 17:09:10 +0300

    refactor Use argparse to define cli params

--- manage.py
+++ manage.py
@@ -3,26 +3,46 @@ import sys
 
 import uvicorn
 
+import argparse
 
-async def run_api():
+argparser = argparse.ArgumentParser(description="RentikuSearch Argparser")
+
+argparser.add_argument(
+    'instance',
+    choices=['api', 'web'],
+    help="instance to run [web, api]",
+)
+argparser.add_argument('-H', "--host", help="host to serve")
+argparser.add_argument('-p', "--port", type=int, help="port to serve")
+argparser.add_argument("-r",
+                       "--reload",
+                       action="store_true",
+                       help="reload server (for dev)")
+
+
+def run_api(host="0.0.0.0", port=5050, reload=False):
     uvicorn.run("RentikuSearch.api.v1.app:app",
-                host="0.0.0.0",
-                port=5050,
-                reload=True)
+                host=host,
+                port=port,
+                reload=reload)
 
 
-async def run_web():
-    uvicorn.run(
-        "RentikuSearch.web_fastapi.app:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-    )
+def run_web(host="0.0.0.0", port=8000, reload=False):
+    uvicorn.run("RentikuSearch.web_fastapi.app:app",
+                host=host,
+                port=port,
+                reload=reload)
+
+
+def main():
+    args = argparser.parse_args()
+    host, port, reload, instance = args.host, args.port, args.reload, args.instance
+
+    if instance == "api":
+        run_api(host, port, reload)
+    elif instance == "web":
+        run_web(host, port, reload)
 
 
 if __name__ == "__main__":
-    choice = sys.argv[1]
-    if choice == "api":
-        asyncio.run(run_api())
-    elif choice == "web":
-        asyncio.run(run_web())
+    main()
